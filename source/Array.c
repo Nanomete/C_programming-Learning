c341ae03d7eb5cfe30b78e817b01a42bb85c58ca
Nawamin ICE <157040537+Nanomete@users.noreply.github.com>
2024-10-14 21:48:35 +0700

    add Array.c

---
+++ source/Array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+   setbuf(stdout, NULL);
+   int i, j, value;
+   int Data[7] = {5, 3, 4, 1, 7, 6, 8};
+   int ArraySize = 7;
+
+   printf("Printf data before sort \n\n");
+
+   for (i = 0; i < ArraySize; i++) {
+      printf(" %d", Data[i]);
+   }
+
+   printf("\n---------------------------\n");
+
+	// command to find the minimum to maximum values
+   for (i = 0; i < ArraySize; i++) {
+      value = Data[i];
+      for (j = i - 1; j >= 0; j--) {
+         if (Data[j] <= value) break;
+         {
+            Data[j + 1] = Data[j];
+         }
+      }
+      Data[j + 1] = value;
+   }
+
+   printf("Print data after sort \n\n");
+
+   for (i = 0; i < ArraySize; i++) {
+      printf(" %d", Data[i]);
+   }
+   
+   return 0;
+}  
